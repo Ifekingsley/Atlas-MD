59b38bb540a67efb8e1bb9dcebbc709f85469c73
Kingsley <100943735+Ife250@users.noreply.github.com>
2023-03-29 16:12:42 +0200

    Update config.js

--- config.js
+++ config.js
@@ -32,20 +32,20 @@
 require("dotenv").config();
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "918101187835";   // You can Change this number //
+  gg = "265889976968";   // You can Change this number //
 }
 
 // -------------------------------------------------------------- //
 
 
-global.owner = gg.split(",");
+global.owner = gg.split("265889976968,");
 global.mongodb = process.env.MONGODB || "mongodb+srv://fantox:xrto71r@cluster0.dnlowts.mongodb.net/?retryWrites=true&w=majority";
 global.sessionId = process.env.SESSION_ID || "ok";
 global.prefa = process.env.PREFIX || "-";
 global.tenorApiKey =
   process.env.TENOR_API_KEY || "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c";
-global.packname = process.env.PACKNAME || `Atlas MD`;
-global.author = process.env.AUTHOR || "by: Team Atlas";
+global.packname = process.env.PACKNAME || `Kingz MD`;
+global.author = process.env.AUTHOR || "by: Team Kingsley";
 global.port = process.env.PORT || "8000";
 
 module.exports = {
